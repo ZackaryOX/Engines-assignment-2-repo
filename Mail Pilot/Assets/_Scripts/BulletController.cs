0d5af35ca025f089942e215e35526cb3e7cc4008
ZackaryOX <zekokhamis@gmail.com>
2019-11-06 17:26:18 -0500

    v1.0 Ready For Submission

---
+++ Mail Pilot/Assets/_Scripts/BulletController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed = 0.1f;
+    public Boundary boundary;
+    private static int IDs = 0;
+    private int MyID;
+
+    //TODO: create a reference to the BulletPoolManager
+
+    void Start()
+    {
+        boundary.Top = 2.45f;
+        MyID = IDs;
+        IDs++;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyID = int.Parse(gameObject.name);
+        if (gameObject.activeSelf == true)
+        {
+            Move();
+            CheckBounds();
+        }
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0.0f, bulletSpeed, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y >= boundary.Top)
+        {
+            //TODO: This code needs to change to use the BulletPoolManager's
+            //TODO: ResetBullet function which will return the bullet to the pool
+            BulletPoolManager.GetInstance().ResetBullet(this.MyID);
+        }
+    }
+}
---
+++ Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+
+
+public class BulletContainer
+{
+    public int ID;
+    public GameObject Bulletobj;
+    public bool IsActive;
+
+    public BulletContainer(int tempid, GameObject tempobj, bool active)
+    {
+        ID = tempid;
+        Bulletobj = tempobj;
+        IsActive = active;
+        Bulletobj.SetActive(active);
+        Bulletobj.name = ID.ToString();
+    }
+    public void SetActive()
+    {
+        IsActive = true;
+        this.Bulletobj.SetActive(true);
+    }
+    public void SetInactive()
+    {
+        this.IsActive = false;
+        this.Bulletobj.SetActive(false);
+    }
+}
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+    private static GameObject BulletPrefab;
+    private static int BulletAmt = 25;
+    private static int BulletsInPool = 0;
+    private static BulletPoolManager Singleton;
+
+    
+
+    private BulletPoolManager()
+    {
+        
+
+    }
+
+    //TODO: create a structure to contain a collection of bullets
+    List<BulletContainer> Bullets = new List<BulletContainer>();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(BulletPrefab == null)
+        {
+            BulletPrefab = bullet;
+        }
+        
+
+        // TODO: add a series of bullets to the Bullet Pool
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet(Vector3 spawnpos, Quaternion temp)
+    {
+        
+        BulletContainer returnval;
+        for(int i = 0; i < BulletAmt; i++)
+        {
+            if(GetInstance().Bullets[i].IsActive == false)
+            {
+                GameObject tempobj = GetInstance().Bullets[i].Bulletobj;
+                tempobj.transform.position = spawnpos;
+                tempobj.transform.rotation = temp;
+                GetInstance().Bullets[i].SetActive();
+                Debug.Log("ISACTIVE?: " + GetInstance().Bullets[i].IsActive);
+                return tempobj;
+            }
+        }
+        return null;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(int id)
+    {
+
+        for (int i = 0; i < BulletAmt; i++)
+        {
+            if (GetInstance().Bullets[i].IsActive == true)
+            {
+                if (GetInstance().Bullets[i].ID == id)
+                {
+
+                    GetInstance().Bullets[i].SetInactive();
+                }
+
+            }
+        }
+    }
+
+    static public BulletPoolManager GetInstance()
+    {
+        if(Singleton == null)
+        {
+            Singleton = new GameObject("Container").AddComponent<BulletPoolManager>();
+            for (int i = 0; i < BulletAmt; i++)
+            {
+                BulletContainer temp = new BulletContainer(BulletsInPool, Instantiate(BulletPrefab), false);
+                BulletsInPool++;
+                GetInstance().Bullets.Add(temp);
+
+            }
+        }
+
+        return Singleton;
+
+    }
+}
